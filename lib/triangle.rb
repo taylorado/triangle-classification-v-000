a454fe1a4ff0c7bd427fbcc704eb8be32263ff73
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 18:32:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  # write code here
+  attr_accessor :Aside, :Bside, :Cside
+
+  def initialize(Aside, Bside, Cside)
+    @Aside = Aside
+    @Bside = Bside
+    @Cside = Cside
+    @triangle_sides = [@Aside, @Bside, @Cside]
+  end
 end
