c71f0c5d5fff00d2a822e822ddbf0ca8d8f5f04e
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 18:41:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,5 +13,21 @@ class Triangle
    sorted.max < sorted.sum
  end
 
+ def kind
+    if valid?
+      case @triangle_sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
 
 end
+
+
+class TriangleError < StandardError
+end
+
