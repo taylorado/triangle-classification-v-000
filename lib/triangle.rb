17feeae03068b6876b679f7dc0375bb622de5efc
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 18:34:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,10 @@
 class Triangle
   attr_accessor :Aside, :Bside, :Cside
 
-  def initialize(Aside, Bside, Cside)
-    @Aside = Aside
-    @Bside = Bside
-    @Cside = Cside
+  def initialize(a_side, b_side, c_side)
+    @Aside = a_side
+    @Bside = b_side
+    @Cside = c_side
     @triangle_sides = [@Aside, @Bside, @Cside]
   end
 end
