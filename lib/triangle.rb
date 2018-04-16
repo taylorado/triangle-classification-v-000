95574e67e3cf618e25c7cffb1798a9bd7a70e713
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 18:36:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,4 +7,12 @@ class Triangle
     @c_side = c_side
     @triangle_sides = [@Aside, @Bside, @Cside]
   end
+
+  def valid?
+   sorted_sides = @sides_array.sort
+   valid_lengths = @sides_array.all? { |side| side >= 0 }
+   valid_lengths && (sorted_sides[0] + sorted_sides[1] > sorted_sides[2])
+ end
+
+ 
 end
